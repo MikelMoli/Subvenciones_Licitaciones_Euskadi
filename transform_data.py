f339c9a2fc8513034d54ffd6fb9ff56ce73dd6bd
MikelMoli <molixnu@gmail.com>
2022-07-13 18:49:58 +0200

    Processing made for sample, but data quality of JSONs is not good. Pending to review the scrapping

--- transform_data.py
+++ transform_data.py
@@ -18,27 +18,27 @@ class TransformAndMergeData:
 
         return full_df
 
-    def add_empty_register_to_dict(self, final_dict):
+    def add_empty_register_to_dict(self, final_dict, i):
         for key in final_dict.keys():
-            final_dict[key].append('-')
+            if len(final_dict[key]) < i+1:
+                final_dict[key].append('-')
         return final_dict
 
-    def handle_adjudication_summary(self, text, final_dict):
-        # url = row['URL']
+    def handle_splitted_lines_text(self, text, final_dict, i):
         splitted_text = text.split("\n")
-        splitted_dict = {}
         for x in splitted_text:
             if ':' not in x:
                 continue
             splitted = x.split(':')
-            if '/' in x:
-                final_dict[splitted[0].lstrip()].append(':'.join(splitted[1:]).lstrip())
-            else: 
-                final_dict[splitted[0].lstrip()].append(splitted[1].lstrip())
+            if len(final_dict[splitted[0]]) < i+1:
+                if '/' in x:
+                    final_dict[splitted[0].lstrip()].append(':'.join(splitted[1:]).lstrip())
+                else: 
+                    final_dict[splitted[0].lstrip()].append(splitted[1].lstrip())
         
         return final_dict
 
-    def fix_dictionary_dimensions(self, final_dict, url, i):
+    def fix_dictionary_dimensions(self, final_dict, i):
         for key, item in final_dict.items():
             if len(item) < i+1:
                 # Check if the logic of adding an empty record here is correct or not
@@ -47,7 +47,7 @@ class TransformAndMergeData:
                 # raise Exception
         return final_dict
 
-    def check_dictionary_dimensions(self, final_dict, url, empty):
+    def check_dictionary_dimensions(self, final_dict, url, empty, i):
         item_len = -1
         for key, item in final_dict.items():
             if item_len == -1:
@@ -55,7 +55,7 @@ class TransformAndMergeData:
                 continue
             elif len(item) != item_len:
                 # Check if the logic of adding an empty record here is correct or not
-                final_dict[key].append('-')
+                print(i)
                 print(f'CHECK IF {key} exists in the next adjudication: {url}')
                 print(f'Empty register: {empty}')
                 raise Exception
@@ -63,34 +63,39 @@ class TransformAndMergeData:
 
     def splitted_lines_to_columns(self, df):
         
-        final_dict = {key: [] for key in utils.new_columns}
+        final_dict = {key: [] for key in utils.NEW_COLUMNS_TO_ADD} # ADD ALL COLUMNS
         for i, row in df.iterrows():
-
-            text = row['Resumen Adjudicación']
+            if i == 0:
+                print('paro')
             url = row['URL']
-            empty = False
-            if text != '-':
-                final_dict = self.handle_adjudication_summary(text, final_dict)
-            else:
-                final_dict = self.add_empty_register_to_dict(final_dict)
-                empty = True
-            final_dict = self.fix_dictionary_dimensions(final_dict, url, i) # Do it with the assert
+            for key in utils.KEYS_TO_SPLIT:
+                text = row[key]
 
-            assert self.check_dictionary_dimensions(final_dict, url, empty)
-        
-        df = pd.concat([df, pd.DataFrame.from_dict(final_dict)], axis=1)
-        return df
+                empty = False
+                if text != '-':
+                    final_dict = self.handle_splitted_lines_text(text, final_dict, i)
+                else:
+                    # HAY QUE CAMBIAR LA LÓGICA DE ESTO PORQUE SI NO VA A AÑADIR 1 EMPTY POR KEY
+                    final_dict = self.add_empty_register_to_dict(final_dict, i)
+                    empty = True
+            
+            final_dict = self.fix_dictionary_dimensions(final_dict, i) # Do it with the assert
+
+            assert self.check_dictionary_dimensions(final_dict, url, empty, i)
+
+        return final_dict
+
 
     def run(self):
-        
         if 'scrapped_data_sample.csv' in os.listdir('./data/'):
             df = pd.read_csv('./data/scrapped_data_sample.csv')
         else:
             df = self.get_complete_df()
         
-        KEYS_TO_SPLIT = ['Resumen Adjudicación', 'Empresas Licitadoras', 'Datos de Adjudicación', 'Otro tipo de poder']
-        df = self.splitted_lines_to_columns(df)
-        df.drop(KEYS_TO_SPLIT, axis=1, inplace=True)
+
+        final_dict = self.splitted_lines_to_columns(df)
+        df = pd.concat([df, pd.DataFrame.from_dict(final_dict)], axis=1)
+        df.drop(utils.KEYS_TO_SPLIT, axis=1, inplace=True)
         df.to_csv('dict_sample.csv', index=False)
 
 if __name__ == '__main__':
